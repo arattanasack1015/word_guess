369cb5359c32441bbe5971cbdcb9c67da8047809
kyuu <arattanasack@gmail.com>
2019-10-13 18:14:49 -0700

    Creates game within function

--- assets/js/guess.js
+++ assets/js/guess.js
@@ -1,70 +1,117 @@
-//Array for fruits
+//Fruit Array
 const fruit = ['orange', 'peach', 'watermelon', 'pineapple', 'pomegranate', 'dragonfruit', 'banana', 'strawberry'];
 
-//Global Variables
+
+//Empty variables to store values later
 let randomFruit = "";
 let fruitLetters = []
-let underScore = [];
-let blanksAndCorrect = [];
+let underscore = 0;
+let correctLetters = [];
 let wrongGuess = [];
 
 //Counter Variables
 let wins = 0;
 let losses = 0;
-let guessesRemaining = 9;
+let livesRemaining = 9;
 
-// DOMs
-docfruitWord = document.getElementsByClassName('fruitWord');
+// DOM
+let docFruitWord = document.getElementsByClassName('fruitWord');
+let docCorrectGuess = document.getElementsByClassName('correctGuess');
+let docWrongGuess = document.getElementsByClassName('wrongGuess');
+let docLives = document.getElementsByClassName('life');
 
 
-//Main Game
-    let underScoreLength = () => {
-        for(let i = 0; i < randomFruit.length; i++) {
-            underScore.push(' _ ');
-            docfruitWord[0].innerHTML = underScore.join(' ');
-        }
-        return underScore;
-    }
-function wordGame() {
+
+// game
+function game() {
+    // RNG from fruit array
     randomFruit = fruit[Math.floor(Math.random() * fruit.length)];
 
+    // converts word into letter array 
     fruitLetters = randomFruit.split("");
+
+    // stores length of word in underscore, for later use
+    underscore = fruitLetters.length;
+
+    // creates underscores for each letter
+    for (let i = 0; i < underscore; i++) {
+        correctLetters.push("_");
+    }
+
+    //showing the "_" within HTML
+    docFruitWord[0].innerHTML = "  " + correctLetters.join("  ");
+
+    //console logging 
+    console.log('randomFruit', randomFruit);
+    console.log('fruitLetters', fruitLetters)
+    console.log('underscore', underscore)
+    console.log('correctLetters', correctLetters)
 }
 
-// function checkLetters(letter) {
-//     var fruitLetters = false;
-//     for (var i = 0; i < underScore; i++) {
-//         if (randomFruit[i] == letter) {
-//             fruitLetters = true;
-//         }
-//     }
-//     if (fruitLetters) {
-//         for (var i = 0; i < underScore; i++) {
-//             if (randomFruit[i] == letter) {
-//                 blanksAndCorrect[i] = letter;
-//             }
-//         }
-//     }
-//     else {
-//         wrongGuess.push(letter);
-//         guessesRemaining--;
-//     }
-//     console.log(blanksAndCorrect);
+// compares input to answer key
+function fruitCheck(letter) {
+    let letterCheck = false;
+    //if the generated randomword is equal to the letter entered... then variable is true
+    for (let i = 0; i < underscore; i++) {
+        if (fruitLetters[i] == letter) {
+            letterCheck = true;
+        }
+    }
+    //if letterCheck (false)
+    if (letterCheck) {
+        //check each letter to see if it matches word
+        for (let i = 0; i < underscore; i++) {
+            if (fruitLetters[i] == letter) {
+                correctLetters[i] = letter;
+            }
+        }
+    }
+    //otherwise, push the incorrect guess in the wrong guesses section, and reduce remaining guesses
+    else {
+        wrongGuess.push(letter);
+        docWrongGuess[0].innerHTML = wrongGuess.join(' ');
+        livesRemaining--;
 
-// }
+        docLives[0].innerHTML = livesRemaining.join(' ');
+    }
+    console.log(correctLetters);
+}
+
+// reset
+function reset() {
+    livesRemaining = 9;
+    wrongGuess = [];
+    correctLetters = [];
+    game()
+}
+
+function end() {
+    console.log("wins:" + wins + "| losses:" + losses + "| guesses left:" + livesRemaining)
+
+    if (fruitLetters.toString() == correctLetters.toString()) {
+        wins++;
+        reset();
+        alert("Wow! you've won " + wins + " times!");
+    } else if (livesRemaining === 0) {
+        losses++;
+        reset();
+        alert("Yikes... you need to read up on your fruits.");
+    }
+}
 
 
 
-wordGame()
 
-// //check for keyup, and convert to lowercase then store in guesses
-// document.onkeyup = function (event) {
-//     var guesses = String.fromCharCode(event.keyCode).toLowerCase();
-//     //check to see if guess entered matches value of random word
-//     checkLetters(guesses);
-//     //process wins/loss 
-//     // complete();
-//     //store player guess in console for reference 
-//     console.log(guesses);
 
-//     //display/store incorrect letters on screen
+game();
+
+
+
+//Listens for letter inputs
+document.addEventListener('keypress', (event) => {
+    let keyWord = String.fromCharCode(event.keyCode);
+    fruitCheck(keyWord);
+    end();
+    console.log(keyWord);
+    
+});
